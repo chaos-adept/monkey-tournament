555d364e24d9d0a4367f31a57f66e5aa409a77bc
Denis Rykovanov <denis.rykovanov@mail.ru>
2016-08-13 20:39:29 +0300

    [+] # added logging for attempts in by slf4js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { startServer } from './lib/server';
+
+
 const  httpPort = process.env.port || 3000;
 const  httpHost = process.env.host || 'localhost';
 startServer({ httpPort, httpHost });
